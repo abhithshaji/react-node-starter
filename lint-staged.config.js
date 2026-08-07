0abbf65d7268aaa4c0d119d2d5d3cfda38c7441f
abhithshaji <abhithshaji@gmail.com>
2026-08-07 08:17:39 +0000

    fix: linstaged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,21 +2,21 @@ const config = {
   // Client specific rules
   "packages/client/**/*.{js,ts,tsx}": [
     "pnpm --filter client lint:eslint-fix",
-    "prettier --write"
+    "pnpm run format:fix"
   ],
   "packages/client/**/*.css": [
     "pnpm --filter client lint:stylelint-fix",
-    "prettier --write"
+    "pnpm run format:fix"
   ],
 
   // Server specific rules
   "packages/server/**/*.{js,ts}": [
     "pnpm --filter server lint:eslint-fix",
-    "prettier --write"
+    "pnpm run format:fix"
   ],
 
   // Root and global configurations
-  "*.{json,html,yaml,yml,md}": ["prettier --write"]
+  "*.{json,html,yaml,yml,md}": ["pnpm run format:fix"]
 };
 
 export default config;
