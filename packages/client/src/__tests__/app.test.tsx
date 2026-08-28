5d8b459056b3650c1e659ce1ecbe0eec5546a508
abhithshaji <abhithshaji@gmail.com>
2026-08-28 14:44:18 +0000

    fix(client): vitest to work with react-router

--- packages/client/src/__tests__/app.test.tsx
+++ packages/client/src/__tests__/app.test.tsx
@@ -1,9 +1,14 @@
 import { describe, it } from "vitest";
 import { render } from "@testing-library/react";
 import App from "@/App.tsx";
+import { MemoryRouter } from "react-router";
 
 describe("App", () => {
   it("App Renders", () => {
-    render(<App />);
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
   });
 });
